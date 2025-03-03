87ce0ef908fa5ee1c51aff26c677063cb2236cc5
Montero-MD <matthewdavid.montero@wvsu.edu.ph>
2025-03-03 19:46:58 +0800

    refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,80 @@ import Search from './Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 
+const navItems = [
+  {
+    label: "Our Brands",
+    menuItems: [
+      {
+        href: "#", label: "Brands", subItems: [
+          { href: "#", label: "Skin Barrier Health / Atopic Dermatitis" },
+          { href: "#", label: "Acne / Acne Prone Skin" },
+          { href: "#", label: "Fungal Infectoin / Scalp Health" },
+          { href: "#", label: "Ageing Skin" },
+          { href: "#", label: "Sun Protection" },
+          { href: "#", label: "Hyperhidrosis" },
+          { href: "#", label: "Cleansing" },
+        ]
+      },
+      {
+        href: "#", label: "Innovative Ingredients", subItems: [
+          { href: "#", label: "Spherulite" },
+          { href: "#", label: "Redensyl" },
+        ]
+      },
+      { href: "#", label: "Regulatory Compliance" },
+      { href: "#", label: "Product Standards" },
+    ]
+  },
+  {
+    label: "Our Initiatives",
+    menuItems: [
+      { href: "#", label: "New Products" },
+      { href: "#", label: "Integrated Distribution" },
+      { href: "#", label: "Patient Resources with Practitioners / Medical Societies" },
+      { href: "#", label: "PRIDE" },
+      { href: "#", label: "DRIVE" },
+      { href: "#", label: "INSTIGATTER" }
+    ]
+  },
+  {
+    label: "Our Story",
+    menuItems: [
+      { href: "#", label: "Who We Are and What We Do" },
+      { href: "#", label: "Milestones" },
+      { href: "#", label: "Growth" },
+      { href: "#", label: "Structure and Leadership" },
+    ]
+  },
+  {
+    label: "Your Resources",
+    menuItems: [
+      {
+        href: "#", label: "HealthCare Partners", subItems: [
+          { href: "#", label: "Local & International Derm Meetings" },
+          { href: "#", label: "Starter Dose Requests" },
+          { href: "#", label: "Integu Special Offers" },
+          { href: "#", label: "Contact Us For Anything" },
+        ]
+      },
+      {
+        href: "#", label: "Patients", subItems: [
+          { href: "#", label: "Find a Dermatologist" },
+          { href: "#", label: "Free EskeenSultation" },
+          { href: "#", label: "Ask Us Anything About Skin" },
+        ]
+      },
+      { href: "#", label: "Investors, Partners, Suppliers" },
+    ]
+  },
+  {
+    label: "Your Feedback"
+  },
+  {
+    label: "Your Career"
+  }
+];
+
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -19,82 +93,18 @@ const Header = () => {
       <div className="container mx-auto px-4 md:px-8 flex items-center justify-between"> {/* Increased padding for larger screens */}
         {/* Logo */}
         <Logo />
-
+        {/* Desktop Menu */}
         <div className="hidden md:flex items-center space-x-6 md:flex">
           <nav className="flex items-center space-x-6">
+          {navItems.map((navItem, index) => (
             <NavItem
-              label="Our Brands"
-              menuItems={[
-                {
-                  href: "#", label: "Brands", subItems: [
-                    { href: "#", label: "Skin Barrier Health / Atopic Dermatitis" },
-                    { href: "#", label: "Acne / Acne Prone Skin" },
-                    { href: "#", label: "Fungal Infectoin / Scalp Health" },
-                    { href: "#", label: "Ageing Skin" },
-                    { href: "#", label: "Sun Protection" },
-                    { href: "#", label: "Hyperhidrosis" },
-                    { href: "#", label: "Cleansing" },
-                  ]
-                },
-                {
-                  href: "#", label: "Innovative Ingredients", subItems: [
-                    { href: "#", label: "Spherulite" },
-                    { href: "#", label: "Redensyl" },
-                  ]
-                },
-                { href: "#", label: "Regulatory Compliance" },
-                { href: "#", label: "Product Standards" },
-              ]}
+              key={index}
+              label={navItem.label}
+              menuItems={navItem.menuItems}
             />
-            <NavItem
-              label="Our Initiatives"
-              menuItems={[
-                { href: "#", label: "New Products" },
-                { href: "#", label: "Integrated Distribution" },
-                { href: "#", label: "Patient Resources with Practitioners / Medical Societies" },
-                { href: "#", label: "PRIDE" },
-                { href: "#", label: "DRIVE" },
-                { href: "#", label: "INSTIGATTER" }
-              ]}
-            />
-            <NavItem
-              label="Our Story"
-              menuItems={[
-                { href: "#", label: "Who We Are and What We Do" },
-                { href: "#", label: "Milestones" },
-                { href: "#", label: "Growth" },
-                { href: "#", label: "Structure and Leadership" },
-              ]}
-            />
-            <NavItem
-              label="Your Resources"
-              menuItems={[
-                {
-                  href: "#", label: "HealthCare Partners", subItems: [
-                    { href: "#", label: "Local & International Derm Meetings" },
-                    { href: "#", label: "Starter Dose Requests" },
-                    { href: "#", label: "Integu Special Offers" },
-                    { href: "#", label: "Contact Us For Anything" },
-                  ]
-                },
-                {
-                  href: "#", label: "Patients", subItems: [
-                    { href: "#", label: "Find a Dermatologist" },
-                    { href: "#", label: "Free EskeenSultation" },
-                    { href: "#", label: "Ask Us Anything About Skin" },
-                  ]
-                },
-                { href: "#", label: "Investors, Partners, Suppliers" },
-              ]}
-            />
-            <NavItem
-              label="Your Feedback"
-            />
-            <NavItem
-              label="Your Career"
-            />
-          </nav>
-        </div>
+          ))}
+        </nav>
+      </div>
 
 
         {/* Mobile Menu Button */}
@@ -111,49 +121,14 @@ const Header = () => {
       {/* Mobile Menu */}
       <div className={`md:hidden absolute top-full left-0 w-full bg-white text-gray-800 shadow-lg ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
         <nav className="flex flex-col p-4">
-          <NavItem
-            label="Our Brands"
-            menuItems={[
-              { href: "#", label: "Brands" },
-              { href: "#", label: "Innovative Ingredients" },
-              { href: "#", label: "Regulatory Compliance" },
-              { href: "#", label: "Product Standards" },
-            ]}
-          />
-          <NavItem
-            label="Our Initiatives"
-            menuItems={[
-              { href: "#", label: "New Products" },
-              { href: "#", label: "Integrated Distribution" },
-              { href: "#", label: "Patient Resources with Practitioners / Medical Societies" },
-              { href: "#", label: "PRIDE" },
-              { href: "#", label: "DRIVE" },
-              { href: "#", label: "INSTIGATTER" }
-            ]}
-          />
-          <NavItem
-            label="Our Story"
-            menuItems={[
-              { href: "#", label: "Who We Are and What We Do" },
-              { href: "#", label: "Milestones" },
-              { href: "#", label: "Growth" },
-              { href: "#", label: "Structure and Leadership" },
-            ]}
-          />
-          <NavItem
-            label="Your Resources"
-            menuItems={[
-              { href: "#", label: "HealthCare Partners" },
-              { href: "#", label: "Patients" },
-              { href: "#", label: "Investors, Partners, Suppliers" },
-            ]}
-          />
-          <NavItem
-            label="Your Feedback"
-          />
-          <NavItem
-            label="Your Career"
-          />
+          {navItems.map((navItem, index) => (
+            <NavItem
+              key={index}
+              label={navItem.label}
+              menuItems={navItem.menuItems}
+              className="mb-4"
+            />
+          ))}
         </nav>
       </div>
     </header>
